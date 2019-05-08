22b5b9ecf0ff991e48dff9ed72626bcd6ca6d92b
Llorenç Comellas <llorenscomellas@gmail.com>
2019-05-08 21:58:04 +0200

    update wep-app, logic, apis

--- staff/llorenç-comellas/web-app/index.js
+++ staff/llorenç-comellas/web-app/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const bodyParser = require('./body-parser')
+const render = require('./render')
+const logic = require('./logic')
 
 const favicon = require('serve-favicon')
 const path = require('path')
-
 
 const { argv: [, , port] } = process
 
@@ -16,28 +17,14 @@ let user = {}
 
 let isLogin = false
 
-function render(body) {
-    return `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
-        ${body}
-    </body>
-    </html>`
-}
 app.get('/', (req, res) =>
     res.send(render(`<a href="/login">login</a><a href="/register">register</a>`))
 
 )
 
 app.get('/register', (req, res) => {
-    res.send(render(`<form method="post" action="/register">
+    res.send(render(`<h2>Register</h2>
+        <form method="post" action="/register">
             <input type="text" name="username" placeholder="username" required >
             <input type="password" name="password" placeholder="password" required>
             <button>Register</button>
@@ -53,7 +40,8 @@ app.post('/register', bodyParser, (req, res) => {
 
     if (username && password) res.send(render(`<p>Ok, user correctly registered, you can now proceed to <a href="/login">login</a></p>`))
     else {
-        res.send(render(`<form method="post" action="/register" >
+        res.send(render(`
+        <form method="post" action="/register" >
             <input type="text" name="username" placeholder="username" >
             <input type="password" name="password" placeholder="password" >
             <button>Register</button>
@@ -62,7 +50,8 @@ app.post('/register', bodyParser, (req, res) => {
 })
 
 app.get('/login', (req, res) =>
-    res.send(render(`<form method="post" action="/login">
+    res.send(render(`<h2>Login</h2>
+        <form method="post" action="/login">
             <input type="text" name="username" placeholder="username">
             <input type="password" name="password" placeholder="password">
             <button>Login</button>
